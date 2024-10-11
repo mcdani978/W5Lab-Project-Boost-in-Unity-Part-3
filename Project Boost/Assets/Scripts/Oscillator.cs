190bd10a936fb0d4783e4da995ad47dea7d158f4
mcdani978 <mcdani978@morrisville.edu>
2024-10-11 19:21:47 -0400

    3.29 Protect Against NaN Error

--- Project Boost/Assets/Scripts/Oscillator.cs
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     Vector3 startPosition;
-   [SerializeField] Vector3 movementVector;
-   [SerializeField] [Range(0,1)] float movementFactor;
-   [SerializeField] float period = 2f;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField][Range(0, 1)] float movementFactor;
+    [SerializeField] float period = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +19,16 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= float.Epsilon) { return; }
+
         float cycles = Time.time / period;
-        
-        const float tau = MathF.PI * 2;
+
+        const float tau = Mathf.PI * 2;
         float rawSinWave = Mathf.Sin(cycles * tau);
 
         movementFactor = (rawSinWave + 1f) / 2f;
 
         Vector3 offset = movementVector * movementFactor;
-        transform.position = startPosition + offset;    
-
-        
+        transform.position = startPosition + offset;
     }
 }
