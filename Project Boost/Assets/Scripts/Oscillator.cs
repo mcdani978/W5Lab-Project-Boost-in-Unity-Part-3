751028396b0b7ea4902698c69d63ac5b95b334c2
mcdani978 <mcdani978@morrisville.edu>
2024-10-11 19:10:29 -0400

    3.28 Mathf.Sin() For Oscillation

--- Project Boost/Assets/Scripts/Oscillator.cs
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class Oscillator : MonoBehaviour
     Vector3 startPosition;
    [SerializeField] Vector3 movementVector;
    [SerializeField] [Range(0,1)] float movementFactor;
+   [SerializeField] float period = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,16 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;
+        
+        const float tau = MathF.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = (rawSinWave + 1f) / 2f;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startPosition + offset;    
+
+        
     }
 }
